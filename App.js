a07a3af7b847196f8b6750d2ae37ebeb54d26aa5
toanelnino1902 <toan.nq194386@gmail.com>
2022-04-19 19:18:12 +0700

    init project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Music Player</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export default App;
